fbc0c6fa4252eca1d35d809c4a27dbb9f6c5edde
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2022-08-10 19:13:26 +0530

    updated basic frontend projects

---
+++ Calculator/index.js
@@ -0,0 +1,26 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    let count = 0;
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            document.querySelector('input').value = "";
+            string = "";
+        }
+        else if(e.target.innerHTML =='()'){
+            string.
+            document.querySelector('input').value = string; 
+            string +=")";
+        }
+        else{
+            console.log(e.target);
+            string += e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+       
+    })
+})
